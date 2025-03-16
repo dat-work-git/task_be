1a46f5a9091b9cc245de9d27b75856323f0ee11f
thanhdat2311 <datmkt2311@gmail.com>
2025-03-16 12:34:29 +0000

    Update them file bi mat do onedrive

--- salemanagement/src/main/java/com/project/salemanagement/congifurations/WebSecurityConfig.java
+++ salemanagement/src/main/java/com/project/salemanagement/congifurations/WebSecurityConfig.java
@@ -5,20 +5,11 @@ import com.project.salemanagement.models.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
